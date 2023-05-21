3e8a579dfc423968f2e0c0e185fcea60ac2418d6
jewelrana3 <juyelrana7752@gmail.com>
2023-05-21 10:52:55 +0600

    vercel

--- index.js
+++ index.js
@@ -31,15 +31,16 @@ async function run() {
     const newCollection = client.db('newChild').collection('child')
     const bookingData = client.db('newChild').collection('bookings')
 
-    app.get('/allData',async(req,res)=>{
-      const result = await newCollection.find().toArray()
+    app.get('/child',async(req,res)=>{
+      const cursor = newCollection.find()
+      const result = await cursor.toArray()
       res.send(result);
     })
-    app.get('/allData/:id',async(req,res)=>{
+    app.get('/child/:id',async(req,res)=>{
       const id = req.params.id;
       const query = {_id: new ObjectId(id)}
       const options = {
-        projection: {  name: 1, price: 1 },
+        projection: {  title: 1, price: 1,photo_url:1,service_id:1 },
       };
       const result = await newCollection.findOne(query,options)
       res.send(result)
@@ -47,9 +48,9 @@ async function run() {
     });
 
     // booking data
-    app.get('/booking',async(req,res)=>{
+    app.get('/bookings',async(req,res)=>{
       console.log(req.query.email)
-      let query = {}
+      let query = {};
       if(req.query?.email){
         query = {email:req.query.email}
       }
@@ -63,7 +64,29 @@ async function run() {
       const result = await bookingData.insertOne(body)
       res.send(result)
 
-    })
+    });
+
+    app.delete('/bookings/:id',async(req,res)=>{
+      const id = req.params.id;
+      const query = {_id:new ObjectId(id)}
+      const result = await bookingData.deleteOne(query);
+      res.send(result);
+    });
+
+    app.patch('/bookings/:id', async (req, res) => {
+      const id = req.params.id;
+      const filter = { _id: new ObjectId(id) };
+      const updated = req.body;
+      console.log(updated);
+      const updateDoc = {
+          $set: {
+              status: updated.status
+          },
+      };
+      const result = await bookingData.updateOne(filter,updateDoc)
+      res.send(result)
+     
+  })
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
